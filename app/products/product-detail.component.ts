c2a313fe0a8034f7f14aa3b56faffe0d0d3e3812
Asad Naeem <drasadnaeem@gmail.com>
2017-01-23 10:48:36 +0500

    no message

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,26 @@
+
+import {Component} from 'angular2/core';
+import { RouteParams, Router} from 'angular2/router';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    templateUrl: 'app/products/product-detail.component.html'
+})
+export class ProductDetailComponent{
+    pageTitle: string = 'Product Detail' ;
+    product: IProduct;
+    errorMessage: string;
+
+    constructor(private _routeParams: RouteParams,
+                private _router: Router,
+                private _productService: ProductService)
+    {
+        let id = +this._routeParams.get('id');
+        this.pageTitle += `: ${id}`;
+    }
+
+    onBack(): void{
+        this._router.navigate(['Products']);
+    }
+}
